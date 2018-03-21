f3bb76cbd4312ca85f9c6b7e3448de7ed7450cbb
tbm2 <tbm2@G3C01.windows.cin.ufpe.br>
2018-03-21 14:47:55 -0300

    Ainda não acabei, tem que quebrar a linha

--- Lista02/src/lista02/TheLongNight.java
+++ Lista02/src/lista02/TheLongNight.java
@@ -1,9 +1,42 @@
 package lista02;
+import java.util.Scanner;
 
 public class TheLongNight {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		
+		String linha;
+		int tamanho, valorCarac;
+		char carac;
+		
+		while(in.hasNextLine()) {
+			linha = in.nextLine();
+			tamanho = linha.length();
+			
+			for(int cont = 0; cont < tamanho; cont++) {
+				carac = linha.charAt(cont);
+				if(carac == ' ') {
+					System.out.print(carac);
+				} else if(carac >= 'A' && carac <= 'T') {
+					valorCarac = carac + 6;
+					carac = (char) valorCarac;
+					System.out.print(carac);
+				} else if(carac > 'T' && carac <= 'Z') {
+					valorCarac = carac - 20;
+					carac = (char) valorCarac;
+					System.out.print(carac);
+				} else if(carac >= 'a' && carac <= 't') {
+					valorCarac = carac + 6;
+					carac = (char) valorCarac;
+					System.out.print(carac);
+				} else if(carac > 't' && carac <= 'z') {
+					valorCarac = carac - 20;
+					carac = (char) valorCarac;
+					System.out.print(carac);
+				} 
+			}
+		}
 
 	}
 
