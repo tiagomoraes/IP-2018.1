722dc91a6e00ada24aacc74f8e1af0214e983305
tiago <tiago@DESKTOP-9PSIVEP>
2018-03-25 00:23:29 -0300

    Está dando certo para os exemplos(ele quer a soma dos copos de cerveja)

--- Lista02/src/lista02/InnCrossroads.java
+++ Lista02/src/lista02/InnCrossroads.java
@@ -6,17 +6,19 @@ public class InnCrossroads {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
-		int x, exp, fat, cont, numT, calcFat, i;
+		int x, exp, fat, cont, numT, i, digit2;
 		double termo, soma, resultado;
 		
 		x = in.nextInt();
-		numT = in.nextInt();
+		numT = in.nextInt() - 1;
+		digit2 = numT + 1;
 		cont = 1;
 		soma = 0;
 		exp = 2;
 		fat = 3;
+		resultado = 0;
 		
-		while(x != 0 || numT != 0) {
+		while(x != 0 || digit2 != 0) {
 			while(numT > 0) {
 				i = fat-1;
 				while(i > 0) {
@@ -39,11 +41,16 @@ public class InnCrossroads {
 				exp+=2;
 				fat = exp + 1;
 			}
-			resultado = x - soma;
-			System.out.printf("%.10f", resultado);
+			resultado += Math.abs(x - soma);
 			x = in.nextInt();
-			numT = in.nextInt();
+			numT = in.nextInt() - 1;
+			digit2 = numT + 1;
+			cont = 1;
+			soma = 0;
+			exp = 2;
+			fat = 3;
 		}
+		System.out.printf("%.10f", resultado);
 
 	}
 
