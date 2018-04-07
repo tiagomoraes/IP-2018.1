e5cfd5f1666b8443e2d0a48353aca5f650719d09
tiago <tiago@DESKTOP-9PSIVEP>
2018-04-07 11:18:27 -0300

    Resolvi o HomemSer com o bubble sort

--- Lista03/src/lista03/HomemSer.java
+++ Lista03/src/lista03/HomemSer.java
@@ -6,8 +6,55 @@ public class HomemSer {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
-		//https://www.devmedia.com.br/entendendo-o-algoritmo-bubble-sort-em-java/24812
-		//Bubble Sort Java
+		int n, aux;
+		String aux2, letra1, letra2;
+		char carac1, carac2;
+		n = in.nextInt();
+		
+		String[] soldados;
+		int[] alturas;
+		
+		soldados = new String[n];
+		alturas = new int[n];
+		
+		for(int i = 0; i < n; i++) {
+			soldados[i] = in.next();
+			alturas[i] = in.nextInt();
+		}
+			
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < (n-1); j++) {
+				if(alturas[j] > alturas[j+1]){
+					aux = alturas[j];
+					aux2 = soldados[j];
+					alturas[j] = alturas[j+1];
+					soldados[j] = soldados[j+1];
+					alturas[j+1] = aux;
+					soldados[j+1] = aux2;
+				} else if(alturas[j] == alturas[j+1]) {
+					letra1 = ""+soldados[j].charAt(0);
+					letra2 = ""+soldados[j+1].charAt(0);
+					letra1 = letra1.toLowerCase();
+					letra2 = letra2.toLowerCase();
+					carac1 = letra1.charAt(0);
+					carac2 = letra2.charAt(0);
+					
+					if(carac1 > carac2) {
+						aux = alturas[j];
+						aux2 = soldados[j];
+						alturas[j] = alturas[j+1];
+						soldados[j] = soldados[j+1];
+						alturas[j+1] = aux;
+						soldados[j+1] = aux2;
+					}
+				}
+			}
+		}
+		
+		for(int i = 0; i < n-1; i++) {
+			System.out.print(soldados[i]+" ");
+		}
+		System.out.print(soldados[n-1]);
 
 	}
 
