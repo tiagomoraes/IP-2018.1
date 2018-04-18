9ff003b1a710ff5ae6cac38d3c5057969ddcdeb7
tbm2 <tbm2@g1c39.windows.cin.ufpe.br>
2018-04-18 09:51:03 -0300

    só criei o arquivo

---
+++ Lista04/src/lista04/ReconstruindoComunicacao.java
@@ -0,0 +1,13 @@
+package lista04;
+import java.util.Scanner;
+
+public class ReconstruindoComunicacao {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		
+
+	}
+
+}
