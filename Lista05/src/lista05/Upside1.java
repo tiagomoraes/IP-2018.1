e9eaa751e11a3ada768b502547797b9ee8341abc
Tiago de Barros Moraes <tbm2@WINDOWS.CIN.UFPE.BR>
2018-05-07 09:51:30 -0300

    Finalizei a lista obrigatória

--- Lista05/src/lista05/Upside1.java
+++ Lista05/src/lista05/Upside1.java
@@ -11,16 +11,36 @@ public class Upside1 {
 		int posX, posY;
 		posX = posY = 0;
 		
+		
 		for(int i = 0; i < 10; i++) {
 			for(int j = 0; j < 10; j++) {
 				labirinto[i][j] = in.nextInt();
 			}
 		}
+		
+		if(move(labirinto, posX, posY, 'a')) {
+			System.out.println("RUN!");
+		} else {
+			System.out.println("Welcome to the Upside Down.");
+		}
 	}
 	
-	public static int move(int[]labirinto, int posX, int posY) {
-		
-		if()
+	public static boolean move(int[][]labirinto, int x, int y, char ult) {
+		boolean resposta;
+		if(x == 9 && y == 9) {
+			resposta = true;
+		} else if(x < 9 && labirinto[x+1][y] == 1 && ult != 's') {
+			resposta = move(labirinto, x+1, y, 'n');
+		} else if(y < 9 && labirinto[x][y+1] == 1 && ult != 'l') {
+			resposta = move(labirinto, x, y+1, 'o');
+		} else if(x > 0 && labirinto[x-1][y] == 1 && ult != 'n') {
+			resposta = move(labirinto, x-1, y, 's');
+		} else if(y > 0 && labirinto[x][y-1] == 1 && ult != 'o') {
+			resposta = move(labirinto, x, y-1, 'l');
+		} else {
+			resposta = false;
+		}
+		return resposta;
 	}
 
 }
