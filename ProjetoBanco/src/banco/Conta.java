99c02b63cf27a9b6a1acb8c6538a2e9b36503d70
Tiago de Barros Moraes <tbm2@windows.cin.ufpe.br>
2018-05-23 11:46:32 -0300

    Add o projeto do banco realizado durante as aulas

---
+++ ProjetoBanco/src/banco/Conta.java
@@ -0,0 +1,28 @@
+package banco;
+
+public class Conta {
+	private String numero;
+	private double saldo;
+	
+	public Conta (String numero) {
+		this.numero = numero;
+		this.saldo = 0;
+	}
+	
+	public double getSaldo() {
+		return this.saldo;
+	}
+	
+	public String getNumero() {
+		return this.numero;
+	}
+	
+	public void creditar(double valor) {
+		this.saldo += valor;
+	}
+	
+	public void debitar(double valor) {
+		this.saldo -= valor;
+	}
+
+}
